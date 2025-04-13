0c37a29ffecb91503a4304a15a50dcab396b2929
Robert Fischer <robert.fischer@artisan-studios.com>
2025-04-12 23:50:40 -0400

    Audioboom also has CORS problems

--- src/hooks/matchups.ts
+++ src/hooks/matchups.ts
@@ -5,7 +5,6 @@ import { useState } from "react";
 const podcastUrls = shuffle(uniq([
   "https://anchor.fm/s/ea4895dc/podcast/rss",
   "https://anchor.fm/s/ff612a4c/podcast/rss",
-  "https://audioboom.com/channels/4322549.rss",
   "https://feed.cdnstream1.com/zjb/feed/download/56/66/3d/56663d4c-d661-4806-a679-ccea6058f935.xml",
   "https://feed.podbean.com/themagicianswife/feed.xml",
   "https://feed.skeptoid.com",
